39f85a3b2fc773f6fbaadc0b46ad97f378521767
antonio <antonyejoao@gmail.com>
2025-02-03 17:55:09 -0300

    adicionando lógca do campo para inserir nomes

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+const campoAdicionarNome = document.getElementById('amigo');
+const campoListaAmigos = document.getElementById('resultado')
+let listaAmigos = []
+
+function adicionarAmigo() {
+    if(campoAdicionarNome.value == 0 || campoAdicionarNome.value < 3) {
+        campoAdicionarNome.setAttribute('placeholder', 'O campo não pode ser vazio ou menor que 3 dígitos.') ;
+        campoAdicionarNome.classList.add('add-name')
+    } else {
+        listaAmigos.push(campoAdicionarNome.value)
+        campoAdicionarNome.innerHTML = listaAmigos
+        //console.log(listaAmigos)
+    }
+}
