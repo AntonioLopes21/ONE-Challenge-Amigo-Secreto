9e4b50ae0ea608a58e7a1c2fd4e0a460ebd19309
Antônio Lopes <antonyejoao@gmail.com>
2025-02-06 01:12:08 -0300

    resolvendo a lógica de remoção do nome da lista assim que for sorteado

--- app.js
+++ app.js
@@ -1,32 +1,44 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 const campoAdicionarNome = document.getElementById('amigo');
 const campoListaAmigos = document.getElementById('listaAmigos')
+const divAmigoSorteado = document.querySelector('.nome_sorteado')
+let quantidadeNomes;
+
 let listaAmigos = [];
 let listaAmigosSorteados = [];
 
 function adicionarAmigo() {
-    if (campoAdicionarNome.length < 3 || campoAdicionarNome.value == 0) {
-        campoAdicionarNome.setAttribute('placeholder', 'O campo não pode ser vazio ou menor que 3 dígitos.');
+    let campoDoNome = campoAdicionarNome.value;
+
+    if (campoDoNome.length < 3 || campoDoNome.length == 0) {
+        //alert("O campo não pode ter menos que 3 dígitos")
         campoAdicionarNome.classList.add('add-name')
+        campoAdicionarNome.setAttribute('placeholder', 'O campo não pode ser vazio ou menor que 3 dígitos.');
+
     } else {
         listaAmigos.push(campoAdicionarNome.value)
         //console.log(listaAmigos)
-        campoListaAmigos.innerHTML = listaAmigos 
-
+        campoListaAmigos.innerHTML = listaAmigos.map(amigo => `<li>${amigo}</li>`).join('') 
     }
 }
 
 function sortearAmigo() {
-    let quantidadeNomes = 0;
-    let listaAmigosSorteados = []
+    let nomeSalvo;
+    let indice;
 
-    if (quantidadeNomes == listaAmigos.length) {
+    if (listaAmigos.length == 0) {
         alert('Todos os amigos foram sorteados! Adicione novos nomes.')
         listaAmigos = []
     } else {
-        
-        console.log(gerarNomeAleatorio(listaAmigos))
-        quantidadeNomes ++;
+        nomeSalvo = gerarNomeAleatorio(listaAmigos)
+        divAmigoSorteado.innerHTML = `O amigo sorteado foi: ${nomeSalvo}`
+        indice = listaAmigos.indexOf(nomeSalvo)
+    }
+
+    if(listaAmigos.includes(nomeSalvo)) {
+        listaAmigos.splice(indice, 1)
+        console.log(`lista de amigos\n${listaAmigos}`)
+
     }
 }
 
